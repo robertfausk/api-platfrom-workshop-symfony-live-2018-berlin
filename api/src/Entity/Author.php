9698a970c0b838106c508cb7d35a86df94fd72c2
robertfausk <robertfreigang@gmx.de>
2018-10-24 12:16:07 +0200

    display name of Author instead of resourceUri in backend
    by using annotation @ApiProperty(iri="http://schema.org/name")
    on property Author::name

--- api/src/Entity/Author.php
+++ api/src/Entity/Author.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -21,6 +22,7 @@ class Author
     /**
      * @var string
      * @ORM\Column
+     * @ApiProperty(iri="http://schema.org/name")
      */
     private $name;
 
